c3ce5c1d36e15bd8772e83e117ab4e7a270c87da
AvadhutPatil45 <audutp99@gmail.com>
2025-08-16 13:19:30 +0530

    My SpringBoot App for learning docker testing

--- MySpringBootSQLApp/src/main/java/in/pwskills/nitin/repository/IUserRepo.java
+++
@@ -1,9 +0,0 @@
-package in.pwskills.nitin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.pwskills.nitin.model.User;
-
-public interface IUserRepo extends JpaRepository<User, Integer>{
-
-}
