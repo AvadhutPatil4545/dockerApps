0cd9dfa2765c9d5e262c97d08c890a85262de0af
AvadhutPatil45 <audutp99@gmail.com>
2025-08-16 13:20:10 +0530

    My SpringBoot App for learning docker testing

---
+++ MySpringBootSQLApp/src/main/java/in/pwskills/nitin/MySpringBootSqlAppApplication.java
@@ -0,0 +1,13 @@
+package in.pwskills.nitin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootSqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootSqlAppApplication.class, args);
+	}
+
+}
